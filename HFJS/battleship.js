6f406f13834d1a1727850f75488bf816fea963e2
JayTheBookworm <jcbyron12@gmail.com>
2018-11-26 11:57:57 -0500

    Add hit verification

--- HFJS/battleship.js
+++ HFJS/battleship.js
@@ -16,4 +16,11 @@ while (isSunk == false) {
     } else {
         guesses = guesses + 1;
     }
+	if (geuss == location1) {
+		hits = hits + 1;
+	} else if (guess == location2) {
+		hits = hits + 1;
+	} else if (guess == location3) {
+		hits = hits + 1;
+	}
 }
