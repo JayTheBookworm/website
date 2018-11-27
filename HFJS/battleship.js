2dfab687b269ab351f4f1fe929b1d34fcc219697
JayTheBookworm <jcbyron12@gmail.com>
2018-11-27 11:38:19 -0500

    Extended hit detection purview

--- HFJS/battleship.js
+++ HFJS/battleship.js
@@ -11,16 +11,30 @@ var isSunk = false;
 while (isSunk == false) {
         
     guess = prompt("Ready, aim, fire! (enter a number from 0-6) :");
+    
     if(guess < 0 || guess > 6) {
-        alert("Please enter a valid cell number!");
+       
+	    alert("Please enter a valid cell number!");
     } else {
-        guesses = guesses + 1;
-    }
-	if (geuss == location1) {
+        
+	    guesses = guesses + 1;
+    
+	if (geuss == location1 || guess == location2 || guess location3) {
+		
 		hits = hits + 1;
-	} else if (guess == location2) {
-		hits = hits + 1;
-	} else if (guess == location3) {
-		hits = hits + 1;
+	alert("HIT!");
+		else {
+			alert("MISS");
+		}
+	if (hits == 3) {
+		
+		isSunk = true;
+		
+		alert("You sank my battleship")
+			}
+		}
 	}
 }
+var stats = "you took " + guesses + " guesses to sink the battleship, " +
+	"which means your shooting accuracy was " + (3/guesses);
+alert(stats) ;
