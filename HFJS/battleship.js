90662d864a753e42ddef761566ed93a8ef7a1fba
JayTheBookworm <jcbyron12@gmail.com>
2018-11-29 11:49:26 -0500

    Fixed spelling errors

--- HFJS/battleship.js
+++ HFJS/battleship.js
@@ -7,23 +7,21 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false) {
-    guess = prompt("Ready, aim, fire! (enter a number from 0-6) :");
-    if(guess < 0 || guess > 6) {  
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    if (guess < 0 || guess > 6) {
 	    alert("Please enter a valid cell number!");
     } else {
-        
 	    guesses = guesses + 1;
-    
-	if (geuss == location1 || guess == location2 || guess == location3) {
-	
-		alert("HIT!")
+    	    
+	    if (guess == location1 || guess == location2 || guess == location3) {
+		alert("HIT!");
 		hits = hits + 1;
 		if (hits == 3) {
-		isSunk = true;
-		alert("You sank my battleship")
-			}
-    } else {
-		alert("MISS!);
+			isSunk = true;
+			alert("You sank my battleship");
+		}
+    	} else {
+		alert("MISS!");
 		}
 	}
 }
